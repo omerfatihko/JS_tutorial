f7c4527ffa640a1ffdf51c2b3305f60aeb6dfeab
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-02-10 22:24:32 +0300

    question and answer validation and error messages added

--- minipollproject/src/components/CreatePollForm.svelte
+++ minipollproject/src/components/CreatePollForm.svelte
@@ -1,8 +1,38 @@
 <script>
     import Button from "../shared/Button.svelte";
     let fields = {question: "", optionA: "", optionB: ""};
+    let errors = {question: "", optionA: "", optionB: ""};
+    let valid = false;
+
     const submitHandler = () => {
-        console.log(fields)
+        valid = true;
+
+        //validate question
+        if (fields.question.trim().length < 5){
+            valid = false;
+            errors.question = "Question must be at least 5 characters long";
+        }else {
+            errors.question = "";
+        }
+        //validate option a
+        if (fields.optionA.trim().length < 1){
+            valid = false;
+            errors.optionA = "Option A cannot be empty";
+        }else {
+            errors.optionA = "";
+        }
+        //validate option b
+        if (fields.optionB.trim().length < 1){
+            valid = false;
+            errors.optionB = "Option B cannot be empty";
+        }else {
+            errors.optionB = "";
+        }
+        //add new poll
+        if (valid){
+            console.log(fields);
+        }
+        
     }
 </script>
 
@@ -10,14 +40,17 @@
     <div class="form-field">
         <label for="question">Enter your question here</label>
         <input type="text" id="question" bind:value={fields.question}>
+        <div class="error">{errors.question}</div>
     </div>
     <div class="form-field">
         <label for="option-a">Enter first option here</label>
         <input type="text" id="option-a" bind:value={fields.optionA}>
+        <div class="error">{errors.optionA}</div>
     </div>
     <div class="form-field">
         <label for="option-b">Enter second option here</label>
         <input type="text" id="option-b" bind:value={fields.optionB}>
+        <div class="error">{errors.optionB}</div>
     </div>
     <Button type="secondary" flat={true}>Add Poll</Button>
 </form>
@@ -38,5 +71,10 @@
     label{
         margin: 10px auto;
         text-align: left;
+    }
+    .error{
+        font-weight: bold;
+        font-size: 12px;
+        color: #d91b42;
     }
 </style>
