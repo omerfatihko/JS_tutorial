08c5f42c55eace2adc5d6049b56a5692807225c1
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-02-11 23:10:38 +0300

    poll animation and transitions added

--- minipollproject/src/components/PollList.svelte
+++ minipollproject/src/components/PollList.svelte
@@ -1,6 +1,8 @@
 <script>
-    import PollStore from "../stores/PollStore.js"
+    import PollStore from "../stores/PollStore.js";
     import PollDetails from "./PollDetails.svelte";
+    import {fade, slide, scale} from "svelte/transition";
+    import {flip} from "svelte/animate";
     
     //export let polls = [];
 
@@ -11,7 +13,7 @@
 
 <div class="poll-list">
     {#each $PollStore as poll (poll.id)}
-        <div>
+        <div in:fade out:scale|local animate:flip={{duration: 500}}>
             <PollDetails {poll}/>
         </div>
     {/each}
