0ad51e8907f8d81e472bd0ca386b3161971a90d4
Fatih Konar <omerfatihk@gmail.com>
2024-08-21 21:12:45 +0300

    if switch and ternary statements

--- Js/js/main.js
+++ Js/js/main.js
@@ -42,3 +42,126 @@ console.log(Math.min(2,5,9));
 console.log(Math.max(2,5,9));
 console.log(Math.random());
 console.log(Math.floor(Math.random()*10)+1);
+
+//code to return a random letter from my name
+const myName = "Omer Fatih Konar";
+len = myName.length;
+rnd = Math.floor(Math.random()*len);
+console.log(myName.charAt(rnd));
+
+//if statements
+let customerIsBanned = false;
+let soup = "mercimek çorbası";
+let crackers = true;
+let reply;
+if (customerIsBanned) {
+    reply = "No soup for you!"
+} else if (soup && crackers) {
+    reply = `Here's your order of ${soup} & crackers.`;
+} else if (soup) {
+    reply = `Here's your order of ${soup}.`;
+} else {
+    reply = "Sorry we are out of soup.";
+}
+console.log(reply);
+
+let testScore = 59;
+let grade;
+let collegeStudent = true;
+
+if (testScore >= 90) {
+    grade = "A";
+} else if (testScore >=80) {
+    grade = "B";
+} else if (testScore >=70) {
+    grade = "C";
+} else if (testScore >=600) {
+    grade = "D";
+} else {
+    if (collegeStudent) {
+        grade = "U";
+    } else {
+        grade = "F"
+    }
+}
+console.log(grade)
+
+//switch statements
+switch (Math.floor(Math.random()*4 + 1)) {
+    case 1:
+        console.log(1);
+        break;
+    
+    case 2:
+        console.log(2);
+        break;
+
+    case 3:
+        console.log(3);
+        break;
+    
+    default:
+        console.log("No match");
+}
+
+let playerOne = "scissors";
+let computer = "rock";
+
+switch (playerOne) {
+    case computer:
+        console.log("Tie!");
+        break;
+    case "rock":
+        if (computer == "paper") {
+            console.log("computer wins!");
+        } else {
+            console.log("player wins!");
+        }
+        break;
+    case "paper":
+        if (computer == "scissors") {
+            console.log("computer wins!");
+        } else {
+            console.log("player wins!");
+        }
+        break;
+    default:
+        if (computer == "rock") {
+            console.log("computer wins!");
+        } else {
+            console.log("player wins!");
+        }
+        break;
+}
+
+//ternary operator
+
+soup = "Mercimek"
+response = soup ? "Yes we have soup." : "Sorry no soup today.";
+console.log(response)
+customerIsBanned = false;
+let soupAccess = customerIsBanned 
+? "No soup for you." 
+: soup 
+? `Yes we have ${soup} today` 
+: "Sorry no soup today.";
+console.log(soupAccess);
+
+testScore = 79;
+grade = 
+testScore >= 90 ? "A": 
+testScore >= 80 ? "B": 
+testScore >= 70 ? "C": 
+testScore >= 60 ? "D":
+"F";
+console.log(grade);
+
+playerOne = "rock";
+computer = "scissors";
+let result = 
+playerOne === computer ? "Tie!":
+playerOne === "rock" && computer === "paper" ? "Computer wins.":
+playerOne === "paper" && computer === "scissors" ? "Computer wins.":
+playerOne === "scissors" && computer === "rock" ? "Computer wins.":
+"Player wins.";
+console.log(result)
