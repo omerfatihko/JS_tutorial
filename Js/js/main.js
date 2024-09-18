561d5bdd7cc6da7e82104a75ac4248b384498217
Fatih Konar <omerfatihk@gmail.com>
2024-09-18 22:20:55 +0300

    classes

--- Js/js/main.js
+++ Js/js/main.js
@@ -1,4 +1,39 @@
-//objects
+/* //Factory function
+function pizzaFactory(pizzaSize) {
+    const crust = "original";
+    const size = pizzaSize;
+    return {
+        bake: () => console.log(`Baking a ${size} ${crust} crust pizza.`)
+    };
+}
+
+const myPizza = pizzaFactory("small");
+myPizza.bake(); */
+
+//js classes
+class Pizza {
+    crust = "original";
+    #sauce = "traditional";
+    #size;
+    constructor(pizzaSize) {
+        this.#size = pizzaSize;
+    }
+    getCrust() {
+        return this.crust;
+    };
+    setCrust(pizzaCrust) {
+        this.crust = pizzaCrust;
+    };
+    hereYouGo() {
+        console.log(`Here's your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza`);
+    };
+}
+
+const myPizza = new Pizza("large");
+myPizza.hereYouGo();
+console.log(myPizza);
+
+/* //objects
 const myObj = {name: "Fatih"};
 console.log(myObj.name)
 
@@ -54,7 +89,7 @@ console.log(vocals);
 function sings({vocals}) {
     return `${vocals} sings!`
 }
-console.log(sings(band));
+console.log(sings(band)); */
 
 //Arrays
 /* const myArray = ["A","B","C","D","E","F"];
